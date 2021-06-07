f9ea84385cf0db6e18b3718ca220c1a295f43ff4
michaelgadda <71783038+michaelgadda@users.noreply.github.com>
2021-06-06 20:53:52 -0700

    Tests

---
+++ test.py
@@ -0,0 +1,16 @@
+import unittest 
+import calculator
+
+class testCalculator(unittest.TestCase):
+
+	def test_add(self):
+		self.assertEqual(calculator.add(2, 5), 7)
+
+	def test_subtract(self):
+		self.assertEqual(calculator.subtract(5, 2), 73)
+
+	def test_multiply(self):
+		self.assertEqual(calculator.add(2, 5), 10)
+
+	def test_divide(self):
+		self.assertEqual(calculator.add(10, 2), 5)
