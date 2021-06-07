c13a30fff92bbe510dbf45aa7ce0ce2886b1db40
michaelgadda <71783038+michaelgadda@users.noreply.github.com>
2021-06-06 20:55:51 -0700

    lint

--- test.py
+++ test.py
@@ -1,16 +1,18 @@
-import unittest 
+import unittest
 import calculator
 
 class testCalculator(unittest.TestCase):
+	
 
-	def test_add(self):
-		self.assertEqual(calculator.add(2, 5), 7)
+    def test_add(self):
+        self.assertEqual(calculator.add(2, 5), 7)
 
-	def test_subtract(self):
-		self.assertEqual(calculator.subtract(5, 2), 73)
+    def test_subtract(self):
+        self.assertEqual(calculator.subtract(5, 2), 73)
 
-	def test_multiply(self):
-		self.assertEqual(calculator.add(2, 5), 10)
+    def test_multiply(self):
+        self.assertEqual(calculator.add(2, 5), 10)
 
-	def test_divide(self):
-		self.assertEqual(calculator.add(10, 2), 5)
+
+    def test_divide(self):
+        self.assertEqual(calculator.add(10, 2), 5)
