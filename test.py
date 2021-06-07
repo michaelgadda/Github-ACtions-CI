42ef312b05688712e3b43a1e387b88170b7192b1
michaelgadda <71783038+michaelgadda@users.noreply.github.com>
2021-06-06 21:09:34 -0700

    stuff

--- test.py
+++ test.py
@@ -8,13 +8,13 @@ class testCalculator(unittest.TestCase):
         self.assertEqual(calculator.add(2, 5), 7)
 
     def test_subtract(self):
-        self.assertEqual(calculator.subtract(5, 2), 73)
+        self.assertEqual(calculator.subtract(5, 2), 3)
 
     def test_multiply(self):
-        self.assertEqual(calculator.add(2, 5), 10)
+        self.assertEqual(calculator.multiply(2, 5), 10)
 
     def test_divide(self):
-        self.assertEqual(calculator.add(10, 2), 5)
+        self.assertEqual(calculator.divide(10, 2), 5)
 
 
 if __name__ == '__main__':
